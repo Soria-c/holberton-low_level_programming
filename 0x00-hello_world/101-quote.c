9c6d7a11e5fec087e70ef952e95149227e9da744
ENki <enki@localhost.localdomain>
2022-02-03 13:07:22 -0500

    Script that prints to the output without using printf() or puts()

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(97);
+	putchar(110);
+	putchar(100);
+	putchar(32);
+	putchar(116);
+	putchar(104);
+	putchar(97);
+	putchar(116);
+	putchar(32);
+	putchar(112);
+	putchar(105);
+	putchar(101);
+	putchar(99);
+	putchar(101);
+	putchar(32);
+	putchar(111);
+	putchar(102);
+	putchar(32);
+	putchar(97);
+	putchar(114);
+	putchar(116);
+	putchar(32);
+	putchar(105);
+	putchar(115);
+	putchar(32);
+	putchar(117);
+	putchar(115);
+	putchar(101);
+	putchar(102);
+	putchar(117);
+	putchar(108);
+	putchar(34);
+	putchar(32);
+	putchar(45);
+	putchar(32);
+	putchar(68);
+	putchar(111);
+	putchar(114);
+	putchar(97);
+	putchar(32);
+	putchar(75);
+	putchar(111);
+	putchar(114);
+	putchar(112);
+	putchar(97);
+	putchar(114);
+	putchar(44);
+	putchar(32);
+	putchar(50);
+	putchar(48);
+	putchar(49);
+	putchar(53);
+	putchar(45);
+	putchar(49);
+	putchar(48);
+	putchar(45);
+	putchar(49);
+	putchar(57);
+	putchar(10);
+	return (1);
+}
