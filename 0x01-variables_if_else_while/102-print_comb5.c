9dfd9b59af50a0a9ed6231429c615875ede17f0e
ENki <enki@localhost.localdomain>
2022-02-04 17:11:14 -0500

    Refactoring 1 and correction 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,6 @@ int main(void)
 	unsigned short count2 = 48;
 	unsigned short count3 = 48;
 	unsigned short count4 = 48;
-	unsigned short row = 1;
 
 	while (count1 < 58)
 	{
@@ -20,12 +19,9 @@ int main(void)
 			count3 = count1;
 			while (count3 < 58)
 			{
-				if (row == 1)
-				{
+				count4 = 48;
+				if (count3 == count1)
 					count4 = count2 + 1;
-				}
-				else
-					count4 = 48;
 				while (count4 < 58)
 				{
 					putchar(count1);
@@ -33,19 +29,20 @@ int main(void)
 					putchar(32);
 					putchar(count3);
 					putchar(count4);
+					if (count2 == 56 && count1 == 57)
+					{
+						putchar(10);
+						break;
+					}
 					putchar(44);
 					putchar(32);
 					count4++;
-
 				}
-				row = 0;
 				count3++;
 			}
-			row++;
 			count2++;
 		}
 		count1++;
 	}
 	return (0);
 }
-
