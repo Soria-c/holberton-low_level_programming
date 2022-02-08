f4fb068287f385f52a73dc9a689fefa0423161ab
Carlos Soria <cs227772@gmail.com>
2022-02-08 13:48:24 -0500

    Correction 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,27 @@
 /**
- * print_alphabet_x10 - prints the alphabet plus a new line ten times
- *
+ * print_alphabet_x10 - prints "_putchar"
+ * @c: input character
+ * Return: prints a char
  */
+
+int _putchar(char c);
 
 void print_alphabet_x10(void)
 {
 	char alphabet;
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	for (alphabet = 'a'; alphabet <= '{'; alphabet++)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-			_putchar(alphabet);
-		_putchar('\n');
+		i++;
+		if (alphabet == '{')
+		{
+			alphabet = '\n';
+		}
+		_putchar(alphabet);
+		if (alphabet == '\n')
+			alphabet = '`';
+		if (i == 270)
+			break;
 	}
-
 }
