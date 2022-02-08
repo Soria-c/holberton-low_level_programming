98f0c9350dfd121778ac5e2697d5e9db8f774a71
Carlos Soria <cs227772@gmail.com>
2022-02-08 10:59:49 -0500

    Function that prints '_putchar'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,9 @@
-#include "main.h"
-
 /**
- * main - prints "_putchar"
+ * print_putchar - prints "_putchar"
  *
- * Return: 0 when successful
  */
 
-int main(void)
+void print_putchar(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,5 +14,4 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
 }
