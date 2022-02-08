f6f55d7077188d24b4adc6ecd0e7bbc9a975aea3
Carlos Soria <cs227772@gmail.com>
2022-02-08 10:06:43 -0500

    Header file containing function prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,23 @@
 #include "_putchar.c"
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 
 int _putchar(char c);
+void print_alphabet(void);
+
+#endif /* _MAIN_H_ */
+/**
+ * print_alphabet - prints the alphabet plus a new line
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+}
 
