0151a72885e6262934051be59ae8de765c05f63b
Carlos Soria <cs227772@gmail.com>
2022-02-08 12:08:01 -0500

    Header file containing function prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,43 +6,14 @@
 #include "2-print_alphabet_x10.c"
 #include "3-islower.c"
 #include "4-isalpha.c"
+#include "5-sign.c"
 
 int _putchar(char c);
-
-/**
- * print_putchar - prints "_putchar"
- *
- */
 void print_putchar(void);
-
-/**
- * print_alphabet - prints the alphabet plus a new line
- *
- */
 void print_alphabet(void);
-
-/**
- * print_alphabet_x10 - prints the alphabet plus a new line ten times
- *
- */
 void print_alphabet_x10(void);
-
-/**
- * _islower - checks if a character is in lower case
- *@c: char input
- *
- * Return: 1 if lower case, 0 otherwise
- */
 int _islower(char c);
-
-/**
- * _isalpha - checks if a character is alpha
-│*@c: char input
-│*
-│* Return: 1 if alphabetic, 0 otherwise
-*/
-
-
 int _isalpha(char c);
+int print_sign(int n);
 
 #endif /* _MAIN_H_ */
