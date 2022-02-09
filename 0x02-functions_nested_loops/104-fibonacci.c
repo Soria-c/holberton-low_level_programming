e7ca84e9e0c98cce37979abf9862813823862a8e
Carlos Soria <cs227772@gmail.com>
2022-02-09 18:50:22 -0500

    Program that prints the first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -83,10 +83,10 @@ void phi2(unsigned long x, unsigned long y, int i)
 	x1 = x1 / 10000;
 	y2 = y2 + ((y1 % 10000) * 10000);
 	y1 = y1 / 10000;
-	while (i >= 87 && i < 99)
+	while (i >= 87 && i < 98)
 	{
 		i++;
-		if (i < 99)
+		if (i < 98)
 		{
 			if (y2 < 10000000)
 			{
