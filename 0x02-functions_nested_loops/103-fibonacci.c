77364b8f05150ae8b2fe93d896e7ded1a5205a64
Carlos Soria <cs227772@gmail.com>
2022-02-08 22:24:40 -0500

    Program that finds and sums of even fibonacci numbers less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - calls the phi function*
+ * @x: integer
+ * @y: int one step behind x
+ * @sum: sum of even numbers
+ * Return: 0 if successful
+ */
+void phi(unsigned long x, unsigned long y, unsigned long long sum);
+int main(void)
+{
+	phi(2, 1, 0);
+	return (0);
+}
+
+/**
+ * phi - fibonacci sequence
+ * @x: integer
+ * @y: one step behind x
+ * @sum: sum of even numbers
+ */
+void phi(unsigned long x, unsigned long y, unsigned long long sum)
+{
+	if (x < 4000000)
+	{
+		unsigned long long z = x;
+
+		if (x % 2 == 0)
+			sum += x;
+		x = x + y;
+		y = z;
+		phi(x, y, sum);
+	}
+	else
+	{
+		printf("%llu", sum);
+		printf("\n");
+	}
+}
