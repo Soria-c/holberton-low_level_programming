11176594f3a0f045d983c156af59528ac3aaa1b4
Carlos Soria <cs227772@gmail.com>
2022-02-08 22:07:43 -0500

    Program that prints the first 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,34 +4,37 @@
  * main - calls the phi function*
  * @x: integer
  * @y: int one step behind x
+ * @i: counter
  * Return: 0 if successful
  */
-void phi(int x, int y);
+void phi(unsigned long x, unsigned long y, int i);
 int main(void)
 {
-	phi(2, 1);
+	phi(2, 1, 1);
 	return (0);
 }
 
 /**
- * phi - fibonacci sequence sum
+ * phi - fibonacci sequence
  * @x: integer
  * @y: one step behind x
+ * @i: counter
  */
-void phi(int x, int y)
+void phi(unsigned long x, unsigned long y, int i)
 {
-	if (x < 50)
+	i++;
+	if (i <= 50)
 	{
-		int z = x;
+		unsigned long z = x;
 
-		printf("%d, ", y);
+		printf("%lu, ", y);
 		x = x + y;
 		y = z;
-		phi(x, y);
+		phi(x, y, i);
 	}
 	else
 	{
-		printf("%d", y);
+		printf("%lu", y);
 		printf("\n");
 	}
 }
