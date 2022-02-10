7a020b5c78c5f0e15b1c2e6e5fcdc87c9009297a
Carlos Soria <cs227772@gmail.com>
2022-02-10 12:28:55 -0500

    Function that prints a square in the terminal

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,10 +6,10 @@
  */
 void print_square(int size)
 {
-	if (size == 0)
-		_putchar('\n');
 	int i, j;
 
+	if (size == 0)
+		_putchar('\n');
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
