f6f8ed975f20e738e2113060381dbcbb3d22ead2
Carlos Soria <cs227772@gmail.com>
2022-02-10 17:14:11 -0500

    Function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,55 +3,30 @@
 /**
  * print_number - prints the input number
  * @n: input number
- * reverse - reverses the input integer n
- * @i: number of digits
- * Return: reversed number
  */
 
-unsigned int reverse(unsigned int n, int i);
 void print_number(int n)
 {
-	int y, i, j, x, r;
-	unsigned int v = n;
+	int y, i, j, x, o, z;
 
-	y = v;
+	y = n;
+	o = 1;
 	if (n < 0)
 	{
-		v = n * -1;
+		n = n * -1;
 		_putchar('-');
 	}
 	for (i = 0; y != 0; i++)
 		y /= 10;
-	if (v == 0)
-		_putchar('0' + v);
-	r = reverse(v, i);
-	for (j = 0; j < i; j++)
-	{
-		x = r % 10;
-		_putchar('0' + x);
-		r /= 10;
-	}
-}
-/**
- * reverse - reverses the input integer n
- * @n: input integer
- * @i: number of digits of input number
- * Return: reversed number
- */
-unsigned int reverse(unsigned int n, int i)
-{
-	int o, d, j, z, x;
-
-	d = 0;
-	o = 1;
 	for (z = 1; z < i; z++)
 		o *= 10;
+	if (n == 0)
+		_putchar('0' + n);
 	for (j = 0; j < i; j++)
 	{
-		x = (n % 10) * o;
-		d += x;
-		n /= 10;
+		x = n / o;
+		_putchar('0' + x);
+		n = n % o;
 		o /= 10;
 	}
-	return (d);
 }
