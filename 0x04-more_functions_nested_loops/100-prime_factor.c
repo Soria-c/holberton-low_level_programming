dbda0b0628c0ecb9935b04de4c6e186ebbd7796d
Carlos Soria <cs227772@gmail.com>
2022-02-10 19:57:24 -0500

    test

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - prints the largest prime factor of 612852475143
+ * Return: 0 if successful
+ */
+
+
+int main(void)
 {
 	unsigned long int j, min;
 
 	j = 612852475143;
 	min = 2;
-	while(min != 4020)
+	while (min != 4020)
 	{
 		if (j % min != 0)
 			min++;
