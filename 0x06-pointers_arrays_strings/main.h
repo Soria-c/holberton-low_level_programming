27e76192cbd6ad6d825b6586e9b1527d864f30cf
Carlos Soria <cs227772@gmail.com>
2022-02-17 16:30:13 -0500

    Header file containing functions prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *cap_string(char *);
 char *leet(char *);
 int _putchar(char c);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* _MAIN_H_ */
