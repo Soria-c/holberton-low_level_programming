92cf9c68a9f6c29e146d8abd6c5ea01b0191c7fa
Carlos Soria <cs227772@gmail.com>
2022-02-23 13:14:15 -0500

    Function that calculates x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - Calculates x raised to the power of y.
+ * @x: input integer.
+ * @y: input integer.
+ * Return: x raised to the power of y.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
