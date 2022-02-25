335ce04af1a29f02e10ca426f7450a7aa4bab6c0
Carlos Soria <cs227772@gmail.com>
2022-02-25 12:12:07 -0500

    Correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	if (argc <= 2)
-		return (0);
+	if (argc == 1)
+		printf("%d", 0);
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
