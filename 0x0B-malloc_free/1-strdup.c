dc06d47db481d949c50deab0b304d2c9f71134d4
Carlos Soria <cs227772@gmail.com>
2022-03-01 09:10:56 -0500

    Function that duplicates a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,10 +9,12 @@ char *_strdup(char *str)
 	int i, j;
 	char *s;
 
+	if (str == NULL)
+		return (0);
 	for (i = 0; *(str + i) != '\0'; i++)
 		continue;
 	s = malloc(i);
-	if (s == NULL || str == NULL)
+	if (s == NULL)
 		return (0);
 	for (j = 0; j < i; j++)
 		*(s + j) = *(str + j);
