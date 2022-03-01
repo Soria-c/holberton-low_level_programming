1a04d6222821fba553b667d79367eb9903464097
Carlos Soria <cs227772@gmail.com>
2022-03-01 11:44:34 -0500

    Function that creates a 2d integer array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,14 +8,13 @@
 int **alloc_grid(int width, int height)
 {
 	int **m;
-	int j;
+	int j, k, i;
 
 	if (width <= 0 || height <= 0)
 		return (0);
 	m = malloc(8 * height);
 	if (m == NULL)
 	{
-		free(m);
 		return (0);
 	}
 	for (j = 0; j < height; j++)
@@ -23,9 +22,13 @@ int **alloc_grid(int width, int height)
 		m[j] = malloc(4 * width);
 		if (m[j] == NULL)
 		{
-			free(m);
 			return (0);
 		}
+	}
+	for (k = 0; k < height; k++)
+	{
+		for (i = 0; i < width; i++)
+			m[k][i] = 0;
 	}
 	return (m);
 }
