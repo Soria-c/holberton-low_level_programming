426cae411133b1601be61abb8b7c7d4d5aa1c384
Carlos Soria <cs227772@gmail.com>
2022-03-03 09:40:47 -0500

    Refactor

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,13 @@ int *array_range(int min, int max)
 {
 	int *c, i, n;
 
-	if (min > max)
-		return (NULL);
+	/*if (min > max)
+		return (NULL);*/
 	n = max - min;
 	if (n < 0)
 		n = -n;
 	c = malloc((n * 4) + 1);
-	if (c == NULL)
+	if (c == NULL || min > max)
 		return (NULL);
 	for (i = 0; i <= n; i++)
 		*(c + i) = i + min;
