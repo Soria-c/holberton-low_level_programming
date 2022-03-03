109395aa4c5f9541c9d6297540a681609caf3521
Carlos Soria <cs227772@gmail.com>
2022-03-03 12:33:57 -0500

    Function that allocates memory using malloc (memory set to 0)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if ( nmemb == 0 || size == 0)
 		return (NULL);
-	s = malloc((nmemb * size) + 1);
+	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
