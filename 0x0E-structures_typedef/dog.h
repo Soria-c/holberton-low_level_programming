37cd030cfc9ea206cdd8667099b356a509e505ef
Carlos Soria <cs227772@gmail.com>
2022-03-07 13:21:58 -0500

    Header file containing the struct for a new type dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type "dog".
+ * @name: name of the dog.
+ * @owner: name of the owner.
+ * @age: age of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
