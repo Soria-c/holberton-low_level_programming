5a23ca71c620df4b47381c370754f28106193714
Carlos Soria <cs227772@gmail.com>
2022-03-07 14:13:43 -0500

    Function that initializes a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (age < 0)
-		exit(1);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
