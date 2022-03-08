d62062f02617672e14e9ede6063eabab8299a3f7
Carlos Soria <cs227772@gmail.com>
2022-03-07 19:49:04 -0500

    Funtion that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates and initializes a new instance of dog_t.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: name of the owner.
- * Return: address of new instance of dog_t.
+ * free_dog - frees struct address.
+ * @d: struct address.
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
