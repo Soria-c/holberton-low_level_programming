d75bcfbbdb8a5e5f05a4bd2904cdc3ae3b61d047
Carlos Soria <cs227772@gmail.com>
2022-03-09 12:09:15 -0500

    Corrected error trying to atoi elements outside argv

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
