819cc5b852b7592389ff28c7f0aaa80988f3c905
Carlos Soria <cs227772@gmail.com>
2022-03-09 11:52:27 -0500

    Function that selects the correct function to perform the operation asked by the user.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - sums a and b.
+ * @a: integer.
+ * @b: integer.
+ * Return: sum of a between b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts b from a.
+ * @a: integer.
+ * @b: integer.
+ * Return: substraction of b from a.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a by b.
+ * @a: integer.
+ * @b: integer.
+ * Return: product of a between b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b.
+ * @a: integer.
+ * @b: integer.
+ * Return: division of a by b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -  takes modulo b from a.
+ * @a: integer.
+ * @b: integer.
+ * Return: modulo b of a.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
