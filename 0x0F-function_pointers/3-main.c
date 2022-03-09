3ae3409f0b932b7413c32c31d05e8c2253346e72
Carlos Soria <cs227772@gmail.com>
2022-03-09 11:53:57 -0500

    Entry point of the program for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - prints name of the current program.
+ * @argc: number of command line arguments.
+ * @argv: array containing the program command line arguments.
+ * Return: 0 if successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(a, b));
+	return (0);
+}
