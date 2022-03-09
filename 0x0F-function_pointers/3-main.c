2ae71fe0e3b98e859d8125952102eeaeeb54df02
Carlos Soria <cs227772@gmail.com>
2022-03-09 12:35:31 -0500

    Corrected error handling for unsupported operator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if (argv[2][1] != '\0' || argv[2][0] < 33)
+	if (argv[2][1] != '\0' || !(get_op_func(argv[2])))
 	{
 		printf("Error\n");
 		exit(99);
