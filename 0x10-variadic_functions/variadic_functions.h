0ace99d21f83e7123671832016b018921abdc470
Carlos Soria <cs227772@gmail.com>
2022-03-10 09:42:31 -0500

    Header file containing prototypes

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VAR_F
+#define VAR_f
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /*VAR_F*/
