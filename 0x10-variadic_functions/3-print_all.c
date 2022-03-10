3ea887100f11d85b5d818ebfc187a7efb58e18c4
Carlos Soria <cs227772@gmail.com>
2022-03-10 15:04:29 -0500

    Function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,28 +10,39 @@
 
 void print_all(const char * const format, ...)
 {
-	int i, bl;
+	int i, bl, j, f;
 	va_list args;
 	char *cn;
+	char ch[] = {'i', 'c', 'f', 's'};
 
 	va_start(args, format);
-	i = 0;
-	while (format[i] != '\0' && format)
+	i = bl = f = 0;
+	while (format[i] != '\0')
 	{
-		bl = 0;
+		j = 0;
+		while (j < 4 && f == 1)
+		{
+			if (format[i] == ch[j])
+				printf(", ");
+			j++;
+		}
 		switch (format[i])
 		{
 			case 'i':
 				printf("%d", va_arg(args, int));
+				f = 1;
 				break;
 			case 'c':
 				printf("%c", va_arg(args, int));
+				f = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(args, double));
+				f = 1;
 				break;
 			case 's':
 				cn = va_arg(args, char *);
+				f = 1;
 				if (!cn)
 				{
 					printf("(nil)");
@@ -40,12 +51,9 @@ void print_all(const char * const format, ...)
 				printf("%s", cn);
 				break;
 			default:
-				bl = 1;
 				break;
 		}
 		i++;
-		if (!bl && format[i])
-			printf(", ");
 	}
 	va_end(args);
 	printf("\n");
