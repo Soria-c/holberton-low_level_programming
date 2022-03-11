12feaa7bfb48284693c9a6be7a7db8881b2aca1f
Carlos Soria <cs227772@gmail.com>
2022-03-10 19:44:35 -0500

    64-bit program in assembly that prints Hello, World, followed by a new line.

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,14 +1,14 @@
 section .data
-    text db "Hello, World!",10
+    text db "Hello, World",10
  
 section .text
-    global _start
+    global main
  
-_start:
+main:
     mov rax, 1
     mov rdi, 1
     mov rsi, text
-    mov rdx, 14
+    mov rdx, 13
     syscall
  
     mov rax, 60
