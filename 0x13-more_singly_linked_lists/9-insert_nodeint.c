26c0ffe7c38e7466d0b6bf613f6d80f011da0574
Carlos Soria <cs227772@gmail.com>
2022-03-22 08:56:40 -0500

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!idx)
 	{
 		new->n = n;
-		prev = (*head);
+		prev = *head;
 		*head = new;
 		new->next = prev;
 		return (new);
