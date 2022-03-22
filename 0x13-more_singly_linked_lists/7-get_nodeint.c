a0ef3b003da84edbf860a91e0946975d04174b71
Carlos Soria <cs227772@gmail.com>
2022-03-22 01:16:27 -0500

    Now hanldes a index number greater than number of elements

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 	for (i = 0, t = head; i < index; i++, t = t->next)
-		;
+	{
+		if (!t)
+			return (NULL);
+	}
 	return (t);
 }
