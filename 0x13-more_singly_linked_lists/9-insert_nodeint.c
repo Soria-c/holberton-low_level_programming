87176e1cf99a60cd1b527ff337a27275474cf1f3
Carlos Soria <cs227772@gmail.com>
2022-03-22 01:40:22 -0500

    Fixed style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev = (*head);
 		*head = new;
 		new->next = prev;
-		return(new);
+		return (new);
 	}
 	for (i = 0, t = *head; i < idx - 1; i++, t = t->next)
 	{
