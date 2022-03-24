a8ebe5bbf548cf297048fcb7f5c5d7aeaae4d2ca
Carlos Soria <cs227772@gmail.com>
2022-03-24 13:06:43 -0500

    Function that sets the value of a bit to 0 at a given index.

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * pow_2 - computes powers of 2.
+ * @n: exponent.
+ * Return: 2 to the n.
+ */
+unsigned long int pow_2(unsigned int n)
+{
+	unsigned int i;
+	unsigned long int r = 1;
+
+	if (!n)
+		return (1);
+	for (i = 0; i < n; i++)
+		r *= 2;
+	return (r);
+}
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: input number.
+ * @index: input index.
+ * Return: 1 if worked, -1 if an error ocurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+	if (!((*n >> index) & 1))
+		return (1);
+	*n = *n ^ pow_2(index);
+	return (1);
+}
