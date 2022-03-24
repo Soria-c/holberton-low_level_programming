2f6d0e14c30a6a220171ed70ebe2857c8c403284
Carlos Soria <cs227772@gmail.com>
2022-03-24 14:15:50 -0500

    Function that returns the number of bits you would need to flip to get from one number to another.

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - computes number of bits flip to turn n into m.
+ * @m: input integer.
+ * @n: input integer.
+ * Return: number of flips required.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, c = 0;
+
+	if (n == m)
+		return (0);
+	for (i = 0; i < 32; i++, n = n >> 1, m = m >> 1)
+	{
+		if ((n & 1) != (m & 1))
+			c++;
+	}
+	return (c);
+}
