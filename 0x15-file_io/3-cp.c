01036389a75e28e42784178a67750307e263648a
Carlos Soria <cs227772@gmail.com>
2022-03-28 13:44:55 -0500

    Fixed error messages

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -73,7 +73,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_copy(argv[1], argv[2]);
