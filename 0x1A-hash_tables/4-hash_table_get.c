71ceaabc4ba7f3587a757d50eb18f8fa7f2c7f3e
Carlos Soria <cs227772@gmail.com>
2022-05-26 18:47:27 -0500

    Handled more errors

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (!key || !ht)
+	if (!key || !(*key) |!ht)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
