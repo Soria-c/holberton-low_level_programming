b5a3aa5d70eb188b917b907f1b100c19d8fce9c4
Carlos Soria <cs227772@gmail.com>
2022-05-27 08:18:59 -0500

    Fixed style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new;
 
-	if (!key || !(*key) || !value ||!ht)
+	if (!key || !(*key) || !value || !ht)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (hash_table_check(ht, key, index))
