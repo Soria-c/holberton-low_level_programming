2f1eeb9fb1fc1fe1b38976818d80b0a77a47b177
Carlos Soria <soriac367@gmail.com>
2022-08-31 12:32:08 -0500

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * print - prints message
+ * @x: index
+ * @y: value
+ * Return: index
+ */
+int print(int x, int y)
+{
+	printf("Value checked array[%d] = [%d]\n", x, y);
+	return (x);
+}
+
+/**
+ * linear_search - implementation of the algorithm
+ * @array: pointer to array of integers
+ * @size: size of the aray
+ * @value: value to search for
+ * Return: index of the value in array if found, -1 otherwise
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = -1;
+
+	if (!array)
+		return (-1);
+
+	while ((++i < (int)size) && (value != array[i]))
+		print(i, array[i]);
+	return ((i == size) ? -1 : print(i, array[i]));
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH
+#define SEARCH
+
+#include <stdio.h>
+
+int print(int x, int y);
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /*SEARCH*/
